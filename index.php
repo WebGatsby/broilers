d565d116ec455c906b2dd7a36e6221d048a0602e
WebGatsby <36766289+WebGatsby@users.noreply.github.com>
2021-07-25 19:21:18 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://xn--90ailinle2g.xn--p1acf/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://xn--90ailinle2g.xn--p1acf/";
+		</script>
+
+		<p>You are being redirected to <a href="https://xn--90ailinle2g.xn--p1acf/">https://xn--90ailinle2g.xn--p1acf/</a></p>
+	</body>
+</html>
